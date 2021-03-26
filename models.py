1030bf7638d4eb8ed108c2e656ac4a9e14a22db5
Erich Maas <maase0@students.rowan.edu>
2021-03-26 09:57:44 -0400

    Testing different models

--- models.py
+++ models.py
@@ -1,5 +1,39 @@
 import tensorflow as tf
 
+def get_M6_2(num_classes):
+    dropout = 0.5
+    
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(input_shape=(63, 64, 1), kernel_size=3, activation='relu', filters=32, name="conv2D_1_1_input"),
+        #tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_1"),
+
+        tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=128, name="conv2D_2_1"),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_2"),
+
+        tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=512, name="conv2D_3_1"),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_3"),
+
+        tf.keras.layers.Flatten(name="flatten_1"),
+        
+        tf.keras.layers.Dense(4096, name="dense_1"),
+        tf.keras.layers.Dropout(dropout),
+        tf.keras.layers.Dense(4096, name="dense_2"),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.Dense(num_classes,  name="dense_final"),
+
+        #set the dtype to float32 for numerical stability
+        tf.keras.layers.Softmax(dtype="float32", name="softmax_1_output") 
+    ], name="M7_1")
+
+    return model
+
+
 def get_M7_1(num_classes):
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(input_shape=(63, 64, 1), kernel_size=3, activation='relu', filters=32, name="conv2D_1_1_input"),
@@ -24,6 +58,44 @@ def get_M7_1(num_classes):
     ], name="M7_1")
 
     return model
+
+
+def get_M7_1_dropout(num_classes):
+    dropout = 0.5
+    
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(input_shape=(63, 64, 1), kernel_size=3, activation='relu', filters=32, name="conv2D_1_1_input"),
+        #tf.keras.layers.Dropout(0.25),
+
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_1"),
+
+        tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=128, name="conv2D_2_1"),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_2"),
+
+        tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=512, name="conv2D_3_1"),
+        tf.keras.layers.Dropout(dropout),
+        tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=512, name="conv2D_3_2"),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_3"),
+
+        tf.keras.layers.Flatten(name="flatten_1"),
+        
+        tf.keras.layers.Dense(4096, name="dense_1"),
+        tf.keras.layers.Dropout(dropout),
+        tf.keras.layers.Dense(4096, name="dense_2"),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.Dense(num_classes,  name="dense_final"),
+
+        #set the dtype to float32 for numerical stability
+        tf.keras.layers.Softmax(dtype="float32", name="softmax_1_output") 
+    ], name="M7_1")
+
+    return model
+
 
 def get_M7_2(num_classes):
     model = tf.keras.models.Sequential([
@@ -54,31 +126,40 @@ def get_M7_2(num_classes):
     return model
 
 def get_M9(num_classes):
+    dropout = 0.5
+
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(input_shape=(63, 64, 1), kernel_size=3, activation='relu', filters=32, name="conv2D_1_1_input"),
+        tf.keras.layers.Conv2D(input_shape=(63, 64, 1), kernel_size=3, activation='relu', filters=64, name="conv2D_1_1_input"),
         tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_1"),
 
         tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=128, name="conv2D_2_1"),
+        tf.keras.layers.Dropout(dropout),
         tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_2"),
 
         tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=256, name="conv2D_3_1"),
+        tf.keras.layers.Dropout(dropout),
         tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=256, name="conv2D_3_2"),
+        tf.keras.layers.Dropout(dropout),
         tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_3"),
 
         tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=512, name="conv2D_4_1"),
-        tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=512, name="conv2D_4_2"),
+        tf.keras.layers.Dropout(dropout),
         tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_4"),
 
-        tf.keras.layers.Flatten(name="flatten_1"),
+        #tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=512, name="conv2D_4_2"),
+
+        tf.keras.layers.Flatten(name="flatten"),
         
         
         tf.keras.layers.Dense(4096, name="dense_1"),
+        tf.keras.layers.Dropout(dropout),
         tf.keras.layers.Dense(4096, name="dense_2"),
+        tf.keras.layers.Dropout(dropout),
 
         tf.keras.layers.Dense(num_classes,  name="dense_final"),
 
         #set the dtype to float32 for numerical stability
-        tf.keras.layers.Softmax(dtype="float32", name="softmax_1_output") 
+        tf.keras.layers.Softmax(dtype="float32", name="softmax_output") 
     ], name="M9")
 
     return model
