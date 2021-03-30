c9cb5f90c9d12ee6aefb6fd10e596a5f22c61201
Erich Maas <maase0@students.rowan.edu>
2021-03-30 19:45:39 -0400

    Model gets to 80% now

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow.keras.layers as layers
 
 def get_M6_2(num_classes):
     dropout = 0.5
@@ -43,6 +44,7 @@ def get_M6_2(num_classes):
 
 def get_M7_1(num_classes):
     model = tf.keras.models.Sequential([
+        
         tf.keras.layers.Conv2D(input_shape=(64, 64, 1), kernel_size=3, activation='relu', filters=32, name="conv2D_1_1_input"),
         tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_1"),
 
@@ -132,6 +134,57 @@ def get_M7_2(num_classes):
 
     return model
 
+
+
+def get_LargeKernel(num_classes, dropout=0.5):
+
+    model = tf.keras.models.Sequential()
+
+    model.add(layers.experimental.preprocessing.Rescaling(1./255))
+    model.add(layers.experimental.preprocessing.RandomRotation((-0.05,0.05), fill_mode='constant', fill_value=0))
+    model.add(layers.experimental.preprocessing.RandomZoom(height_factor=(0.0, 0.5), fill_mode='constant', fill_value=0))
+    model.add(layers.experimental.preprocessing.RandomTranslation(height_factor=(-0.1,0.1), width_factor=(-0.1,0.1), fill_mode='constant', fill_value=0))
+
+
+    model.add(layers.Conv2D(input_shape=(64, 64, 1), kernel_size=5,  filters=64))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPool2D(pool_size=(2, 2), strides=2))
+
+    model.add(layers.Conv2D(filters=128, kernel_size=3))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPool2D(pool_size=(2, 2), strides=2))
+
+    model.add(layers.Conv2D(filters=256, kernel_size=3))
+    model.add(layers.Conv2D(filters=256, kernel_size=3))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPool2D(pool_size=(2, 2), strides=2))
+
+    model.add(layers.Conv2D(filters=512, kernel_size=3))
+    model.add(layers.Conv2D(filters=512, kernel_size=3))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    #model.add(layers.MaxPool2D(pool_size=(2, 2), strides=2))
+
+    model.add(layers.Flatten())
+
+    model.add(layers.Dense(4096))
+    model.add(layers.Dropout(dropout))
+    model.add(layers.Dense(4096))
+    model.add(layers.Dropout(dropout))
+    model.add(layers.Dense(4096))
+    model.add(layers.Dropout(dropout))
+
+    model.add(layers.Dense(num_classes))
+
+    return model
+
+
+
+
+
 def get_M9(num_classes):
     dropout = 0.5
 
@@ -142,28 +195,28 @@ def get_M9(num_classes):
         tf.keras.layers.experimental.preprocessing.RandomTranslation(height_factor=(-0.1,0.1), width_factor=(-0.1,0.1), fill_mode='constant', fill_value=0),
 
         tf.keras.layers.Conv2D(input_shape=(64, 64, 1), kernel_size=3,  filters=64, name="conv2D_1_1_input", padding='valid'),
-        #tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Activation('relu'),
         tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_1"),
         tf.keras.layers.Dropout(dropout),
 
 
         tf.keras.layers.Conv2D(kernel_size=3, filters=128, name="conv2D_2_1", padding='valid'),
-        #tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Activation('relu'),
         tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_2"),
         tf.keras.layers.Dropout(dropout),
 
         tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=256, name="conv2D_3_1", padding='valid'),
         tf.keras.layers.Conv2D(kernel_size=3,  filters=256, name="conv2D_3_2"),
-        #tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Activation('relu'),
         #tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_3"),
         tf.keras.layers.Dropout(dropout),
 
         tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=512, name="conv2D_4_1", padding='valid'),
         tf.keras.layers.Conv2D(kernel_size=3, filters=512, name="conv2D_4_2"),
-        #tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Activation('relu'),
         tf.keras.layers.Dropout(dropout),
         tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_4"),
@@ -258,6 +311,7 @@ def get_custom(num_classes):
 def get_Github_ref(num_classes):
     #https://github.com/CaptainDario/DaKanjiRecognizer-ML/blob/master/CNN_kanji/jupyter/DaKanjiRecognizer.ipynb
     _model = tf.keras.models.Sequential([
+        tf.keras.layers.experimental.preprocessing.Rescaling(1./255),
         tf.keras.layers.Conv2D(input_shape=(64, 64, 1), kernel_size=3, activation='relu', filters=32, name="conv2D_1_2_input"),
         tf.keras.layers.Conv2D(kernel_size=3, activation='relu', filters=32, name="conv2D_1_1"),
         tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2, name="maxpool_1"),
